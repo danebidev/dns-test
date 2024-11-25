195d2bc7acfcbcf0f3874410f32e12e52053121d
danebidev <57720273+danebidev@users.noreply.github.com>
2024-11-25 19:14:45 +0100

    Add Config struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use once_cell::sync::Lazy;
+use std::{path::PathBuf, sync::Mutex};
+
+struct Config {
+    config_file: PathBuf 
+}
+
+static CONFIG: Lazy<Mutex<Config>> = Lazy::new(||  Mutex::new(Config {
+    config_file: PathBuf::from("")
+}));
+
+fn update_path(path: PathBuf) {
+    let mut cfg = CONFIG.lock().unwrap();
+    cfg.config_file = path;
+}
+
+fn get_path() -> PathBuf {
+    CONFIG.lock().unwrap().config_file.clone()
+}
+
 fn main() {
-    println!("Hello World");
 }
